b38a19dd9888c2cc3f9a2234d93816fe697c6e7a
Vitas11 <vitasvitalie18@gmail.com>
2020-09-09 03:47:39 -0700

    Add Spring Boot skeleton

---
+++ calculator/src/test/java/com/Vitas11/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Vitas11.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
